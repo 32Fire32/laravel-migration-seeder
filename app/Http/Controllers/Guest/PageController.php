68cd59ce781b5fc10ddd93eeedcbbdc1ba7ca5d6
32Fire32 <nicola.gianfelici@hotmail.it>
2023-01-18 10:46:24 +0100

    correzione dopo lezione, implementa Carbon

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -5,12 +5,12 @@ namespace App\Http\Controllers\Guest;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Train;
-
+use Illuminate\Support\Carbon;
 
 class PageController extends Controller
 {
     public function index(){
-        $trains = Train::whereDate('departure_time', '>=', date('2023-01-17'))->get();
+        $trains = Train::whereDate('departure_time', '>=', Carbon::now())->get();
         return view('welcome', compact('trains'));
     }
 }
